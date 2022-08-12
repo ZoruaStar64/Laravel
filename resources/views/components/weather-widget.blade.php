0499545b3ee3e2cc4a3e35f62081759a0abf1454
ZoruaStar64 <sanderb119@gmail.com>
2022-08-12 15:42:57 +0200

    weather api page is pretty much finished

--- resources/views/components/weather-widget.blade.php
+++ resources/views/components/weather-widget.blade.php
@@ -1,5 +1,5 @@
 <?php
-$weatherIcon = $currentWeather['weather'][0]['icon'];
+$currentWeatherIcon = $currentWeather['weather'][0]['icon'];
 ?>
 
 <div class="container flex flex-col mx-auto px-4 py-8 gap-6 text-white w-96 h-fit bg-weatherBG rounded-xl">
@@ -8,7 +8,7 @@ $weatherIcon = $currentWeather['weather'][0]['icon'];
         <div class="ml-10 text-4xl h-12 ">{{round($currentWeather['main']['temp'])}}&#176;C</div>
         <div class="text-l text-gray-300 h-12">Feels like: {{round($currentWeather['main']['feels_like'])}}&#176;C</div>
         <div class="mx-auto">
-            <img class="w-20" src="http://openweathermap.org/img/wn/{{$weatherIcon}}@2x.png" alt="icon">
+            <img class="w-20" src="http://openweathermap.org/img/wn/{{$currentWeatherIcon}}@2x.png" alt="icon">
         </div>
     </div>
 
@@ -18,9 +18,20 @@ $weatherIcon = $currentWeather['weather'][0]['icon'];
         <div class="text-4xl h-12">{{round($currentWeather['main']['temp_max'])}}&#176;C</div>
     </div>
 
-    @foreach ($futureWeather['list'] as $weather)
-    <div class="container grid grid-cols-5 gap-2 mx-auto px-4 py-3 text-white w-72 bg-weatherFG rounded-full shadow-2xl shadow-purple-400/50">
-        <div class="">{{ strtoupper(\Carbon\Carbon::createFromTimestamp($weather['dt'])->format('D')) }}</div>
+    @foreach ($futureWeather['list'] as $index => $weather) 
+ @php
+    $forecastIcon = $futureWeather['list'][$index]['weather'][0]['icon'];
+@endphp
+    <div class="container grid grid-cols-5 grid-rows-2 gap-2 mx-auto px-4 py-3 text-white w-72 bg-weatherFG rounded-full shadow-2xl shadow-purple-400/50">
+        <div class="row-span-2 mt-4">{{ strtoupper(\Carbon\Carbon::createFromTimestamp($weather['dt'])->format('D')) }}</div>
+        <div class="row-span-2"><img class="" src="http://openweathermap.org/img/wn/{{$forecastIcon}}.png" alt="icon"></div>
+        <div class="row-span-2">{{$futureWeather['list'][$index]['weather'][0]['description']}}</div>
+
+        <!-- <div class="ml-4 row-start-1 col-start-4 text-sm">Max:</div>
+        <div class="ml-4 row-start-2 col-start-4 text-sm">Min:</div> -->
+
+        <div class="row-start-1 col-start-5">{{round($futureWeather['list'][$index]['temp']['max'])}}&#176;C</div>
+        <div class="row-start-2 col-start-5">{{round($futureWeather['list'][$index]['temp']['min'])}}&#176;C</div>
     </div>
     @endforeach
 </div>
