39bdadaa0d3ea0907e76c58bfae685e816469ac7
ZoruaStar64 <sanderb119@gmail.com>
2023-05-16 14:50:49 +0200

    Unit tests work now
    Fixed up the todofactory en unit test database settings

--- database/factories/TodoFactory.php
+++ database/factories/TodoFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Arr;
+use App\Models\Category;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Todo>
@@ -20,11 +21,13 @@ class TodoFactory extends Factory
     public function definition()
     {
         $userArray = [1,2];
-        $categoryArray = [1,2,3,4];
+        //$categoryArray = [1,2,3,4];
+        $randomCategory = Category::all()->random();
         $descriptiveArray = ['Hi.', 'This is a test.', 'Description.', 'Number 15 Burger King foot lettuce.'];
         return [
             'user_id' => Arr::random($userArray),
-            'category_id' => Arr::random($categoryArray),
+            //'categories_id' => Arr::random($categoryArray),
+            'categories_id' => $randomCategory,
             'checked' => 0,
             'name' => fake()->name(),
             'description' => Arr::random($descriptiveArray),
--- tests/Feature/DatabaseTest.php
+++ tests/Feature/DatabaseTest.php
@@ -2,6 +2,10 @@
 
 namespace Tests\Feature;
 
+use Database\Seeders\CategorySeeder;
+use Database\Seeders\TodoSeeder;
+use Database\Seeders\UserSeeder;
+use Database\Seeders\TagSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -18,20 +22,31 @@ class DatabaseTest extends TestCase
     public function test_if_users_can_be_created()
     {
         $this->seed(UserSeeder::class);
+
+        $this->assertDatabaseCount('users', 2);
     }
 
     public function test_if_categories_can_be_created()
     {
         $this->seed(CategorySeeder::class);
+
+        $this->assertDatabaseCount('Categories', 4);
     }
 
     public function test_if_tags_can_be_created()
     {
         $this->seed(TagSeeder::class);
+
+        $this->assertDatabaseCount('Tags', 4);
     }
 
     public function test_if_todos_can_be_created()
     {
-        $this->seed(TodoSeeder::class);
+        $this->seed([
+            CategorySeeder::class,
+            TodoSeeder::class
+        ]);
+
+        $this->assertDatabaseCount('Todos', 25);
     }
 }
